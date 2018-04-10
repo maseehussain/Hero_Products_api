54ec7899b7bdbb22696491145bf25babaafed00a
Ma5ee2 <hussainmasee@gmail.com>
2018-04-10 15:07:20 +0100

    reconfigured the seeds file within db and refactored the code

--- db/seeds.js
+++ db/seeds.js
@@ -3,23 +3,41 @@ mongoose.Promise = require('bluebird');
 
 const { dbURI } = require('../config/environment');
 const Product = require('../models/product');
+const Merchant = require('../models/merchant');
 
-mongoose.connect(dbURI, { useMongoClient: true });
+const merchantData = [
+  {
+    company: 'Amazon'
+  },
+  {
+    company: 'Ebay'
+  },
+  {
+    company: 'HMV'
+  }
+];
 
-mongoose.connection.once('open', () => {
-  mongoose.connection.db.dropDatabase();
-
-  const productData = [{
-    product: 'Epson Perfection V550 Photo Scanner with ReadyScan LED Technology',
-    price: '£179'
-  }, {
-    product: 'Canon CanoScan 9000F Mark II Colour Scanner',
-    price: '£159'
-  }];
-
-  Product
-    .create(productData)
-    .then(products => console.log(`${products.length} products created`))
-    .catch(err => console.log(err))
-    .finally(() => mongoose.connection.close());
-});
+mongoose
+  .connect(dbURI, { useMongoClient: true })
+  .then(db => db.dropDatabase())
+  .then(() => Merchant.create(merchantData))
+  .then(Products => {
+    return Product.create([
+      {
+        product: 'Canon CanoScan 9000F Mark II Colour Scanner',
+        price: '£159'
+        company: merchants[1]
+      }, {
+        product: 'Epson Perfection V550 Photo Scanner with ReadyScan LED Technology',
+        price: '£179'
+        company: merchants[2]
+      }, {
+        product: 'Nintendo Switch Console (Neon) with Mario & Rabbids Kingdom Battley',
+        price: '299'
+        company: merchants[3]
+      },
+    ])
+  })
+  .then(foodbanks => console.log(`${foodbanks.length} foodbanks created!`))
+  .catch(err => console.log(err))
+  .finally(() => mongoose.connection.close());
