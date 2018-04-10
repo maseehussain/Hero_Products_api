a9b85d8f1ac451f9e38c6b3b665fc5246ace857b
Ma5ee2 <hussainmasee@gmail.com>
2018-04-10 12:32:47 +0100

    required all other dependencies needed in app.js

--- app.js
+++ app.js
@@ -8,4 +8,25 @@ mongoose.Promise = require('bluebird');
 mongoose.plugin(require('./lib/globalToJSON'));
 mongoose.plugin(require('mongoose-unique-validator'));
 
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const { port, dbURI, env } = require('./config/environment');
+const routes = require('./config/productRoutes');
+const customResponses = require('./lib/customResponses');
+const errorHandler = require('./lib/errorHandler');
+
+mongoose.connect(dbURI, { useMongoClient: true });
+
+if('test' !== env) app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
+
+app.use(customResponses);
+
+app.use('/', routes);
+
+app.use(errorHandler);
+
+server.listen(port, () => console.log(`Express is listening on port ${port}`));
+
 module.exports = app;
