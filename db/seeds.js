7ecc46fafa19fa6bcfcec9b0c613472e630eb545
Ma5ee2 <hussainmasee@gmail.com>
2018-04-10 13:20:28 +0100

    created the data for the seed file and need to seed and test on insomnia if data comes through

--- db/seeds.js
+++ db/seeds.js
@@ -1,2 +1,22 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
+
+const { dbURI } = require('../config/environment');
+const Product = require('../models/product');
+
+mongoose.connect(dbURI, { useMongoClient: true });
+
+mongoose.connection.once('open', () => {
+  mongoose.connection.db.dropDatabase();
+
+  const productData = [{
+    product: 'Trainers',
+    price: '£20'
+  }];
+
+  Product
+    .create(productData)
+    .then(products => console.log(`${products.length} products created`))
+    .catch(err => console.log(err))
+    .finally(() => mongoose.connection.close());
+});
