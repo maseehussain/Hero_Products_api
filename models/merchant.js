09da55ac24f2f48979c56320a2d42a5b96fde771
Ma5ee2 <hussainmasee@gmail.com>
2018-04-10 14:39:04 +0100

    made schema for merchant

---
+++ models/merchant.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const companySchema = new mongoose.Schema({
+  merchant: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Company', merchantSchema);
