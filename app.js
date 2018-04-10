340fd3a1784962d7425cf75e656e25e06508baac
Ma5ee2 <hussainmasee@gmail.com>
2018-04-10 12:31:47 +0100

    required mongoose in app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
 
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.plugin(require('./lib/globalToJSON'));
+mongoose.plugin(require('mongoose-unique-validator'));
 
 module.exports = app;
