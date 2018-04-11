f3ff91b9e9701bea037d822d7b2a7626cd2a9a23
Ma5ee2 <hussainmasee@gmail.com>
2018-04-11 12:21:11 +0100

    removed body-parser from app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ mongoose.Promise = require('bluebird');
 mongoose.plugin(require('./lib/globalToJSON'));
 mongoose.plugin(require('mongoose-unique-validator'));
 
-const bodyParser = require('body-parser');
 const { port, dbURI, env } = require('./config/environment');
 const routes = require('./config/routes');
 const customResponses = require('./lib/customResponses');
@@ -17,7 +16,6 @@ const errorHandler = require('./lib/errorHandler');
 mongoose.connect(dbURI, { useMongoClient: true });
 
 app.use(express.static(`${__dirname}/public`));
-app.use(bodyParser.json());
 
 app.use(customResponses);
 
